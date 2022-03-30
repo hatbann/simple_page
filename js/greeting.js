ac1e20faa510f0312739591cb2c05304cf3ff8a1
hatbann <0urloveis_great@naver.com>
2022-03-30 13:22:08 +0900

    css
    make bgimage full, make some layout

--- js/greeting.js
+++ js/greeting.js
@@ -1,8 +1,10 @@
 const loginForm = document.querySelector('#login-form');
 const loginInput = document.querySelector('#login-form input');
+const login = document.querySelector('.login');
 const greeting = document.querySelector('#greeting');
 
 const HIDDEN_CLASSNAME = 'hidden';
+const FLEX_CLASSNAME = 'flex';
 const USERNAME_KEY = 'username';
 
 //Greeting 표시하기
@@ -17,6 +19,8 @@ function loginSubmit(event) {
   console.dir(event);
   event.preventDefault();
   loginForm.classList.add(HIDDEN_CLASSNAME);
+  login.classList.remove(FLEX_CLASSNAME);
+  loginForm.classList.remove(FLEX_CLASSNAME);
   const username = loginInput.value;
   localStorage.setItem(USERNAME_KEY, username);
   paintGreetings();
@@ -28,5 +32,7 @@ if (savedUserName === null) {
   loginForm.classList.remove(HIDDEN_CLASSNAME); //show the form
   loginForm.addEventListener('submit', loginSubmit);
 } else {
+  login.classList.remove(FLEX_CLASSNAME);
+  loginForm.classList.remove(FLEX_CLASSNAME);
   paintGreetings(); //show the greetings
 }
